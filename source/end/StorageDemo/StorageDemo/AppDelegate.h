a476d16f0d1a842bf70726e64f24bae27f7c0eb8
Chris Risner <github@chrisrisner.com>
2013-03-12 12:34:47 -0700

    Initial checkin

---
+++ source/end/StorageDemo/StorageDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StorageDemo
+//
+//  Created by Chris Risner on 3/12/13.
+//  Copyright (c) 2013 Microsoft DPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ source/end/StorageDemo/StorageDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StorageDemo
+//
+//  Created by Chris Risner on 3/12/13.
+//  Copyright (c) 2013 Microsoft DPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
