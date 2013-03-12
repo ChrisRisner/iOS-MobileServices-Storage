78008b6e0d32e79d707c6e1ddb5982c2eb0912e5
Chris Risner <github@chrisrisner.com>
2013-03-12 12:50:14 -0700

    Adding initial navigation

---
+++ source/end/StorageDemo/StorageDemo/BlobStorageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BlobStorageTableViewController.h
+//  StorageDemo
+//
+//  Created by Chris Risner on 3/12/13.
+//  Copyright (c) 2013 Microsoft DPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlobStorageTableViewController : UITableViewController
+
+@end
---
+++ source/end/StorageDemo/StorageDemo/TableStroageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableStroageTableViewController.h
+//  StorageDemo
+//
+//  Created by Chris Risner on 3/12/13.
+//  Copyright (c) 2013 Microsoft DPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableStroageTableViewController : UITableViewController
+
+@end
